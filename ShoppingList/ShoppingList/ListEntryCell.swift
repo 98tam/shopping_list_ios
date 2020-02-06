8b1b12338a0e3cef7bc004c7f5ba53c7722096fd
Theresa Ganser <S1810237014@students.fh-hagenberg.at>
2020-02-06 22:45:04 +0100

    add whole app

---
+++ ShoppingList/ShoppingList/ListEntryCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListEntryCell.swift
+//  ShoppingList
+//
+//  Created by Theresa Ganser on 02.02.20.
+//  Copyright © 2020 Theresa Ganser. All rights reserved.
+//
+
+import UIKit
+
+class ListEntryCell: UITableViewCell {    
+    @IBOutlet weak var listEntryLabel: UILabel!
+    @IBOutlet weak var unitsLabel: UILabel!
+    @IBOutlet var iconView: UIImageView!
+    @IBOutlet weak var buyButton: UIButton!
+}
